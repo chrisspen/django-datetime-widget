36de2e0b46b1bb180b16edf8f5275cdca79a6f7d
Chris Spencer <chrisspen@gmail.com>
2016-10-20 19:09:22 -0400

    Fixed template settings.

--- datetimewidget/tests/settings.py
+++ datetimewidget/tests/settings.py
@@ -87,3 +87,31 @@ LOGGING = {
         },
     }
 }
+
+# Required for Django>=1.10.
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            '%s/templates' % PROJECT_DIR,
+        ],
+#         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ],
+            'debug': True,
+        },
+    },
+]
